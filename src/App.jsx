746368c17f5b359d73a90e73ee28993ee5433455
Amanuel <beginningoflife2324@gmail.com>
2025-10-23 08:25:23 -0800

    correct naming

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Favorites from "./components/favorites";
+import Favorites from "./components/Favorites";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Profile from "./components/Profile";
